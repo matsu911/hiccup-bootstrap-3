500820f23f155a85bccaac72f796628f38f8db2b
Alexander Zolotko <azolotko@gmail.com>
2013-07-27 13:00:59 +0300

    Updated project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject hiccup-bootstrap "0.1.2"
+(defproject hiccup-bootstrap-3 "0.2.0-SNAPSHOT"
   :description "Hiccup functions for Twitter's Bootstrap"
-  :url "https://github.com/weavejester/hiccup-bootstrap"
+  :url "https://github.com/azolotko/hiccup-bootstrap-3"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.2.1"]
-                 [hiccup "1.0.3"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [hiccup "1.0.4"]
                  [compojure "1.1.5"]])
