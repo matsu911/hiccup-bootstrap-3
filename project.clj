d0f48caa08f91df9aa698f4d24354a4d150dca24
Shigeaki Matsumura <matsu911@gmail.com>
2016-05-17 17:19:24 +0900

    changed project name, description and url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject hiccup-bootstrap-3 "0.2.0-SNAPSHOT"
-  :description "Hiccup functions for Twitter's Bootstrap"
-  :url "https://github.com/azolotko/hiccup-bootstrap-3"
+(defproject matsu911/hiccup-bootstrap-3 "0.2.0-SNAPSHOT"
+  :description "Hiccup functions for Twitter's Bootstrap 3"
+  :url "https://github.com/matsu911/hiccup-bootstrap-3"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
